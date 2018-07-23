720b673f9f06e98d85f6d80ddee7e7886ac4fd4b
melie <melchan298@gmail.com>
2018-07-23 13:47:21 +0700

    lan5

--- js/piroll.js
+++ js/piroll.js
@@ -53,6 +53,7 @@ function runProgressBar() {
 
   });
 }
+if($(".departments").length >0) {
 var count=0;
 $(window).scroll(function() {
   
@@ -65,6 +66,7 @@ $(window).scroll(function() {
     }
   }
 });
+}
 
   $(window).scroll(function (event) {
     var scroll = $(window).scrollTop();
@@ -139,4 +141,22 @@ $('.number').each(function () {
             $(this).text(Math.ceil(now));
         }
     });
+});
+$(document).ready(function(){
+
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
+
 });
