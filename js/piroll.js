e85648f7673e61b43a4453ba0b9bab143bd460ea
melie <melchan298@gmail.com>
2018-07-25 15:49:39 +0700

    lan6

--- js/piroll.js
+++ js/piroll.js
@@ -144,7 +144,6 @@ $('.number').each(function () {
 });
 $(document).ready(function(){
 
-    //Check to see if the window is top if not then display button
     $(window).scroll(function(){
         if ($(this).scrollTop() > 100) {
             $('.scrollToTop').fadeIn();
@@ -152,8 +151,6 @@ $(document).ready(function(){
             $('.scrollToTop').fadeOut();
         }
     });
-
-    //Click event to scroll to top
     $('.scrollToTop').click(function(){
         $('html, body').animate({scrollTop : 0},800);
         return false;
